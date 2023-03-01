845f4e24b9de6f6b7ab959328a1fe43531ce8bbd
Shylesh <shylesharepelly@gmail.com>
2023-03-01 23:07:14 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+const app = require("./app")
+const { request, response } = require('express');
+
+
+app.listen(3000,()=>{
+    console.log("server started at 3000 port")
+})
